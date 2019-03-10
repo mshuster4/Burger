5f22e95e33e5b966c163c50fb1b1f20b04f7af55
Emroze <emrozesd@users.noreply.github.com>
2019-03-10 15:57:41 -0700

    added initial code and files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Juicy Lucy");
+INSERT INTO burgers (burger_name) VALUES ("Buffalo Burger");
+INSERT INTO burgers (burger_name) VALUES ("Impossible Burger");
